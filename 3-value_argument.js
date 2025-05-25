d5c99e9c0bcc98fb7e1ed9fc5991ff8d36a7c400
Lutendo George Maphala <maphalalg@gmail.com>
2025-05-26 00:39:13 +0200

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,9 +1,7 @@
 let value = process.argv[2];
 
-for (let i = 0; i < value; i++) {
-    if (i === 0){
-        console.log("No argument");
-    } else{
-        console.log(value[i]);
-    }
+if (value === undefined) {
+  console.log("No argument");
+} else {
+  console.log(value);
 }

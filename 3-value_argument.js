c44acf3c65e6daf3aee8b639fffb55a8f4a45628
Lutendo George Maphala <maphalalg@gmail.com>
2025-05-26 00:34:04 +0200

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,12 +1,11 @@
 let value = process.argv[2];
 
-let i = 0;
-
-while(i < value){
-    if (value === 0){
-        console.log("No argument");
-    }else{
-        console.log(value)
-    }
-    i++;
+if (value === undefined || isNaN(parseInt(value))) {
+    console.log("Missing number of occurrences");
+}else{
+    let i = 0;
+    while(i < parseInt(value)){
+        console.log(value);
+        i++;
+}
 }
